ade32f50724c9a54420f21d7ff5f59d7887a9641
Joe <joedravarol@gmail.com>
2019-04-13 13:09:06 +0100

    Organise code and add interactive function to buttons

--- script.js
+++ script.js
@@ -1,41 +1,100 @@
-// Creating DOM elements
 const container = document.createElement('div');
 const title = document.createElement('h1');
+const gridSizeBtn = document.createElement('button');
+const randomColorBtn = document.createElement('button');
+const clearBtn = document.createElement('button');
 
-// Adding Content to elements
 let gridSize = 16;
+let paintColor = '#000';
+let className = 'black';
 
-title.textContent = 'Etch A Sketch';
+function init() {
+    // Add Text Content
+    title.textContent = 'Etch A Sketch';
+    gridSizeBtn.textContent = 'Grid Size';
+    randomColorBtn.textContent = 'Random Color';
+    clearBtn.textContent = 'Clear';
 
-for (let i = 0; i < gridSize ** 2; i++) {
-    const div = document.createElement('div');
-    div.style.backgroundColor = '#fff';
-    container.appendChild(div);
+    // Styling Elements
+    title.style.margin = '1em';
+    container.classList.add('container');
+    
+    // Appending Childs
+    document.body.appendChild(title);
+    document.body.appendChild(gridSizeBtn);
+    document.body.appendChild(randomColorBtn);
+    document.body.appendChild(clearBtn);
+    document.body.appendChild(container);
+
+    createGrid();
 }
 
-document.body.appendChild(title);
-document.body.appendChild(container);
+function createGrid() {
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const div = document.createElement('div');
+        div.style.backgroundColor = '#fff';
+        container.appendChild(div);
+    }
 
-// Styling Elements
-title.style.margin = '1em';
-container.classList.add('container');
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-container.addEventListener('mouseover', paint);
+    // Make the cells fit insize the container
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+}
 
 function paint(e) {
     const target = e.target;
 
-    let newColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()})`;
-    let className = 'colored';
+    if (paintColor !== '#000') {
+        paintColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    }
 
     if (!target.className) {
-        target.style.backgroundColor = newColor;
+        target.style.backgroundColor = paintColor;
         target.className = (className);
-    } 
+    } else if (target.className === 'black' && className === 'colored') {
+        target.style.backgroundColor = paintColor;
+        target.className = 'colored';
+    }
 }
 
 function randomColor() {
     return Math.round(Math.random() * 255);
 }
+
+function removeGrid() {
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    createGrid();
+}
+
+function changePaintColor() {
+
+    if (paintColor === '#000') {
+        paintColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+        randomColorBtn.textContent = 'Black';
+        className = 'colored';
+    } else {
+        paintColor = '#000';
+        randomColorBtn.textContent = 'Random Color';
+        className = 'black';
+    }
+}
+
+// Add Event Listener
+container.addEventListener('mouseover', paint);
+clearBtn.addEventListener('click', removeGrid);
+randomColorBtn.addEventListener('click', changePaintColor);
+gridSizeBtn.addEventListener('click', () => {
+    gridSize = prompt('How many sqaures per side would you like on your new grid?');
+
+    while (!(gridSize >= 1) || !(gridSize <= 100)) {
+        gridSize = prompt('Please enter a number between 1 to 100')
+    }
+
+    removeGrid();
+});
+
+init();
