4c5a6b11a8fc96329523b31c44e324be0bacdefd
Joe <joedravarol@gmail.com>
2019-04-13 01:25:53 +0100

    Add paint color function

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ title.style.margin = '1em';
 container.classList.add('container');
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+container.addEventListener('mouseover', paint);
+
+function paint(e) {
+    const target = e.target;
+
+    console.log(darkness);
+    let newColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, ${darkness})`;
+    let className = 'colored';
+
+    if (!target.className) {
+        target.style.backgroundColor = newColor;
+        target.className = (className);
+    } 
+}
+
+function randomColor() {
+    return Math.round(Math.random() * 255);
+}
